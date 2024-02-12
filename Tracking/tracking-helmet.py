cba5b24635b3779564be35a1986abe1a40871175
saada865 <saadm865@gmail.com>
2024-02-12 13:10:39 +0500

    added tracking-helmet.

--- Tracking/tracking-helmet.py
+++ Tracking/tracking-helmet.py
@@ -1,18 +1,27 @@
 import cv2
 from ultralytics import YOLO
 # !pip3 install lap
+from time import time
 
 # Load the YOLOv8 model
 model = YOLO('/Users/saadahmadmalik/Downloads/best.pt')
 
 # Open the video file
-video_path = "/Users/saadahmadmalik/Downloads/production_id_4608276 (2160p).mp4"
+
+video_path = "/Users/saadahmadmalik/Downloads/pexels_videos_2048246 (1080p).mp4"
+
+#pexels-pavel-danilyuk-7817205 (1080p).mp4
+#pexels_videos_2048246 (1080p).mp4
+
 cap = cv2.VideoCapture(video_path)
+
+loop_time = time()
 
 # Loop through the video frames
 while cap.isOpened():
     # Read a frame from the video
     success, frame = cap.read()
+
 
     if success:
         # Run YOLOv8 tracking on the frame, persisting tracks between frames
@@ -20,6 +29,12 @@ while cap.isOpened():
 
         # Visualize the results on the frame
         annotated_frame = results[0].plot()
+
+        fps_text = 1 / (time() - loop_time)
+        print('FPS: {}'.format(fps_text))
+        loop_time = time()
+
+
 
         # Display the annotated frame
         cv2.imshow("YOLOv8 Tracking", annotated_frame)
